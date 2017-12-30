404232e0e5a54d6154b6f2b38973c4374b48b6df
Roger Willian <roger.nizoli@gmail.com>
2017-12-29 22:01:16 -0200

    configuracao inicial projeto 1->N

---
+++ crud_produtos/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('shared.base')
+
+@section('content')
+
+    <div class="panel panel-default">
+
+        <div class="panel-heading">
+            <span>Bem vindo!</span>
+        </div>
+        <div class="panel-body">
+            <span>Sistema gerenciamento de produtos!</span>
+        </div>
+    </div>
+
+@endsection
